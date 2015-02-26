57babbde17937dedacd50294f0d219dfc60175d4
Dmitry Prazdnichnov <dp@bambucha.org>
2015-02-26 17:48:24 +0300

    Update pypi scripts

--- pypi.bat
+++ pypi.bat
@@ -8,6 +8,10 @@ C:\Python27\python.exe setup.py bdist_wininst upload
 C:\Python27_x64\python.exe setup.py bdist_wininst upload
 C:\Python34\python.exe setup.py bdist_wininst upload
 C:\Python34_x64\python.exe setup.py bdist_wininst upload
+C:\Python27\python.exe setup.py bdist_msi upload
+C:\Python27_x64\python.exe setup.py bdist_msi upload
+C:\Python34\python.exe setup.py bdist_msi upload
+C:\Python34_x64\python.exe setup.py bdist_msi upload
 C:\Python27\python.exe setup.py bdist_wheel upload
 C:\Python34\python.exe setup.py bdist_wheel upload
 C:\Python27\python.exe setup.py sdist --formats=gztar upload
--- pypi.sh
+++ pypi.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-python2 setup.py bdist_egg upload
-python3 setup.py bdist_egg upload
-python2 setup.py bdist_wheel upload
-python3 setup.py bdist_wheel upload
-python2 setup.py sdist --formats=gztar upload
-python2 setup.py sdist --formats=zip upload
+python2.7 setup.py bdist_egg upload
+python3.4 setup.py bdist_egg upload
+python2.7 setup.py bdist_wheel upload
+python3.4 setup.py bdist_wheel upload
+python2.7 setup.py sdist --formats=gztar upload
+python2.7 setup.py sdist --formats=zip upload
