f856d9da766abed49631e9fbc2e2748ae8435db0
Dmitry Prazdnichnov <dp@bambucha.org>
2016-06-04 04:59:45 +0300

    Fix build.sh

--- build.sh
+++ build.sh
@@ -8,16 +8,12 @@ virtualenv -p /usr/bin/python2.7 venv2
 source venv2/bin/activate
 python setup.py sdist build
 python setup.py bdist_egg build
-python setup.py bdist_wheel build
 python setup.py sdist upload
 python setup.py bdist_egg upload
-python setup.py bdist_wheel upload
 deactivate
 
 virtualenv -p /usr/bin/python3.5 venv3
 source venv3/bin/activate
 python setup.py bdist_egg build
-python setup.py bdist_wheel build
 python setup.py bdist_egg upload
-python setup.py bdist_wheel upload
 deactivate
