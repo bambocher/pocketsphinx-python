fce806651ebc4b3435ce204a7ba07f57e2271fae
Dmitry Prazdnichnov <dp@bambucha.org>
2016-08-24 03:29:24 +0300

    Add sphinxad support

--- setup.py
+++ setup.py
@@ -11,6 +11,10 @@ except Extension as err:
     from distutils.command.build import build
     from distutils.command.install import install
 
+libraries = []
+
+libsphinxad = []
+
 libsphinxbase = (
     glob('deps/sphinxbase/src/libsphinxbase/lm/*.c') +
     glob('deps/sphinxbase/src/libsphinxbase/feat/*.c') +
@@ -20,8 +24,39 @@ libsphinxbase = (
 
 libpocketsphinx = glob('deps/pocketsphinx/src/libpocketsphinx/*.c')
 
-sb_include_dirs = ['deps/sphinxbase/include', 'deps/sphinxbase/include/sphinxbase']
+sb_sources = (
+    libsphinxbase +
+    ['swig/sphinxbase/sphinxbase.i']
+)
+
+ps_sources = (
+    libsphinxbase +
+    libpocketsphinx +
+    ['deps/pocketsphinx/swig/pocketsphinx.i']
+)
+
+sb_include_dirs = [
+    'deps/sphinxbase/include',
+    'deps/sphinxbase/include/sphinxbase'
+]
+
 ps_include_dirs = ['deps/pocketsphinx/include']
+
+sb_swig_opts = (
+    ['-modern'] +
+    ['-I' + h for h in sb_include_dirs] +
+    ['-Ideps/sphinxbase/swig'] +
+    ['-outdir', 'sphinxbase']
+)
+
+ps_swig_opts = (
+    ['-modern'] +
+    ['-I' + h for h in sb_include_dirs] +
+    ['-I' + h for h in ps_include_dirs] +
+    ['-Ideps/sphinxbase/swig'] +
+    ['-Iswig/sphinxbase'] +
+    ['-outdir', 'pocketsphinx']
+)
 
 define_macros = [
     ('SPHINXBASE_EXPORTS', None),
@@ -33,7 +68,10 @@ define_macros = [
 extra_compile_args = []
 
 if sys.platform.startswith('win'):
+    libsphinxad.append('deps/sphinxbase/src/libsphinxad/ad_win32.c')
+    sb_sources.extend(libsphinxad)
     sb_include_dirs.extend(['deps/sphinxbase/include/win32'])
+    libraries.append('winmm')
     define_macros.extend([
         ('_WIN32', None),
         ('_CRT_SECURE_NO_DEPRECATE', None),
@@ -49,10 +87,16 @@ if sys.platform.startswith('win'):
         '/wd4334'
     ])
 elif sys.platform.startswith('darwin'):
+    libsphinxad.append('deps/sphinxbase/src/libsphinxad/ad_oss.c')
+    sb_sources.extend(libsphinxad)
     sb_include_dirs.extend(['deps/sphinxbase/include/android'])
+    libraries.extend(['asound'])
 else:
     sys.platform.startswith('linux')
+    libsphinxad.append('deps/sphinxbase/src/libsphinxad/ad_pulse.c')
+    sb_sources.extend(libsphinxad)
     sb_include_dirs.extend(['deps/sphinxbase/include/android'])
+    libraries.extend(['pulse', 'pulse-simple'])
     extra_compile_args.extend([
         '-Wno-unused-label',
         '-Wno-strict-prototypes',
@@ -63,36 +107,9 @@ else:
         '-Wno-sign-compare'
     ])
 
-sb_sources = (
-    libsphinxbase +
-    ['deps/sphinxbase/swig/sphinxbase.i']
-)
-
-ps_sources = (
-    libsphinxbase +
-    libpocketsphinx +
-    ['deps/pocketsphinx/swig/pocketsphinx.i']
-)
-
-swig_opts = ['-modern']
-
-sb_swig_opts = (
-    swig_opts +
-    ['-I' + h for h in sb_include_dirs] +
-    ['-outdir', 'sphinxbase']
-)
-
-ps_swig_opts = (
-    swig_opts +
-    ['-I' + h for h in sb_include_dirs] +
-    ['-I' + h for h in ps_include_dirs] +
-    ['-Ideps/sphinxbase/swig'] +
-    ['-outdir', 'pocketsphinx']
-)
-
 setup(
     name='pocketsphinx',
-    version='0.1.0',
+    version='0.1.1',
     description='Python interface to CMU SphinxBase and PocketSphinx libraries',
     long_description=open('README.rst').read(),
     author='Dmitry Prazdnichnov',
@@ -108,6 +125,7 @@ setup(
             sources=sb_sources,
             swig_opts=sb_swig_opts,
             include_dirs=sb_include_dirs,
+            libraries=libraries,
             define_macros=define_macros,
             extra_compile_args=extra_compile_args
         ),
@@ -116,6 +134,7 @@ setup(
             sources=ps_sources,
             swig_opts=ps_swig_opts,
             include_dirs=sb_include_dirs + ps_include_dirs,
+            libraries=libraries,
             define_macros=define_macros,
             extra_compile_args=extra_compile_args
         )
