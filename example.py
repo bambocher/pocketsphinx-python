235eab2716a86c9b4c8fa9b849daba90cea9bae6
Dmitry Prazdnichnov <dp@bambucha.org>
2016-08-29 02:50:09 +0300

    Update example

--- example.py
+++ example.py
@@ -1,28 +1,12 @@
 #!/usr/bin/env python
-import os
+from pocketsphinx import Pocketsphinx
 
-import sphinxbase as sb
-import pocketsphinx as ps
+ps = Pocketsphinx()
+ps.decode()
 
-MODELDIR = 'deps/pocketsphinx/model'
-DATADIR = 'deps/pocketsphinx/test/data'
-
-# Create a decoder with certain model
-config = ps.Decoder.default_config()
-config.set_string('-hmm', os.path.join(MODELDIR, 'en-us/en-us'))
-config.set_string('-lm', os.path.join(MODELDIR, 'en-us/en-us.lm.bin'))
-config.set_string('-dict', os.path.join(MODELDIR, 'en-us/cmudict-en-us.dict'))
-decoder = ps.Decoder(config)
-
-# Decode streaming data.
-decoder.start_utt()
-stream = open(os.path.join(DATADIR, 'goforward.raw'), 'rb')
-while True:
-    buf = stream.read(1024)
-    if buf:
-        decoder.process_raw(buf, False, False)
-    else:
-        break
-decoder.end_utt()
-stream.close()
-print('Best hypothesis segments:', [seg.word for seg in decoder.seg()])
+print('Hypothesis:', ps.hypothesis())
+print('Probability:', ps.probability())
+print('Score:', ps.score())
+print('Segments:', ps.segments())
+print('Confidence:', ps.confidence())
+print('Best:', *ps.best(), sep='\n')
