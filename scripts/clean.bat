64f9f1415267a1bffd8c7f628661c51ce7c36621
Dmitry Prazdnichnov <dp@bambucha.org>
2016-09-09 17:51:17 +0300

    Removed scripts

--- scripts/clean.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-rmdir /s /q build pocketsphinx.egg-info
-rmdir /s /q sphinxbase\__pycache__ pocketsphinx\__pycache__ tests\__pycache__
-rmdir /s /q pocketsphinx\data pocketsphinx\model
-del /s /f /q sphinxbase\*.pyc pocketsphinx\*.pyc tests\*.pyc
-del /s /f /q tests\*.htk tests\*.lat
-del /s /f /q sphinxbase\sphinxbase.py sphinxbase\*.pyd
-del /s /f /q pocketsphinx\pocketsphinx.py pocketsphinx\*.pyd
--- scripts/clean.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-rm -rf build *.egg-info tests/*.{htk,lat}
-rm -rf sphinxbase/{sphinxbase,pocketsphinx}.py {sphinxbase,pocketsphinx}/*.so
-rm -rf pocketsphinx/{data,model}
